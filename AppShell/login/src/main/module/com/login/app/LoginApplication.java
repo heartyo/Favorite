da13f105e81a07a9707dbfc589948dc424db9037
李诚新 <lichengxin-it@bestpay.com.cn>
2020-09-02 17:18:50 +0800

    初始化

---
+++ AppShell/login/src/main/module/com/login/app/LoginApplication.java
@@ -0,0 +1,12 @@
+package com.login.app;
+
+import android.app.Application;
+
+public class LoginApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
